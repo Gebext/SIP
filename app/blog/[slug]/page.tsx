bbea399b582c2786eff7f7cf6aa5ef4d27162f45
TableStar <afraroad@gmail.com>
2024-12-04 19:25:23 +0700

    fix: comment unused vars

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,44 +1,41 @@
-import BlogContent, { currentArticleType } from "@/app/blog/[slug]/components/blog/BlogContent";
+import BlogContent, {
+  currentArticleType,
+} from "@/app/blog/[slug]/components/blog/BlogContent";
 import strapiClient from "@/helper/apiClient";
 
-export default async function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } =  params;
-
+export default async function Page({ params }: { params: { slug: string } }) {
   try {
     console.log("hello");
-    const response = await strapiClient.get(`/articles?filters[slug][$eq]=${slug}`);
+    const response = await strapiClient.get(
+      `/articles?filters[slug][$eq]=${params.slug}`
+    );
     console.log(response.data);
 
     return (
       <div className="mx-auto min-h-screen max-w-7xl px-4 py-8">
-        <BlogContent slugName={slug} />
+        <BlogContent slugName={params.slug} />
       </div>
     );
-    
   } catch (error) {
-    console.error('Error fetching article:', error);// Trigger 404 if an error occurs
+    console.error("Error fetching article:", error); // Trigger 404 if an error occurs
   }
-
 }
 
 export async function generateStaticParams() {
   try {
     // Fetch data from Strapi
     const response = await strapiClient.get("/articles");
-    const articles:currentArticleType = response.data;
+    const articles: currentArticleType = response.data;
     console.log(response);
 
-
-    const paths = articles.data?.map((article) => ({
-      slug: article.slug,
-    }));
-    return paths||[]
+    if (articles) {
+      const paths = articles.data?.map((article) => ({
+        slug: article.slug,
+      }));
+      return paths;
+    }
   } catch (error) {
-    console.log('Error fetching articles for static params:', error);
+    console.log("Error fetching articles for static params:", error);
     return []; // Return empty array if there's an error
   }
-  }
+}
