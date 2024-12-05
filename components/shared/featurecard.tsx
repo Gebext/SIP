03554691231f62d3dd7f256571b8111f92d7f08e
Gebext <gibrand987@gmail.com>
2024-12-05 16:27:48 +0700

    fix: feature card

--- components/shared/featurecard.tsx
+++ components/shared/featurecard.tsx
@@ -6,14 +6,16 @@ interface FeatureCardProps {
   description: string; // Deskripsi dari card
 }
 
-export default function FeatureCard({ icon, title, description }: FeatureCardProps) {
+export default function FeatureCard({
+  icon,
+  title,
+  description,
+}: FeatureCardProps) {
   return (
     <div className="border-none shadow-lg hover:shadow-xl transition-shadow">
       <div className="p-6">
         <div className="flex items-start gap-4">
-          <div className="p-2 rounded-lg bg-primary/10">
-            {icon}
-          </div>
+          <div className="p-2 rounded-lg bg-primary/10">{icon}</div>
           <div>
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
             <p className="text-muted-foreground text-[14px]">{description}</p>
