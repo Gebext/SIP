9d5b3e9309c11292c64632168e8c6e97561c19d8
TableStar <afraroad@gmail.com>
2024-12-05 12:55:01 +0700

    fix: remove unused import

--- app/blog/[slug]/components/blog/BlogContent.tsx
+++ app/blog/[slug]/components/blog/BlogContent.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
